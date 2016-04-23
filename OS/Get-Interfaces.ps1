c0668d0a5bfbe28db74479c559f14d6e6fd89caf
Michael Foley <roleyfoley@gmail.com>
2016-04-23 21:56:12 +1000

    Removed AutoConfig IP's

--- OS/Get-Interfaces.ps1
+++ OS/Get-Interfaces.ps1
@@ -3,16 +3,16 @@ function Get-NicIPs {
         $Adapters = Get-NetAdapter -Physical | Where-Object { $_.Status -eq "Up" } 
                
         foreach ( $Adapter in $Adapters ) {
-        $IPDetails = Get-NetIPAddress -InterfaceIndex $($Adapter.InterfaceIndex)
-        $Interface = New-Object psobject
-        $Interface | Add-Member -MemberType NoteProperty -Name Name -Value $($Adapter.Name)
-        $Interface | Add-Member -MemberType NoteProperty -Name MacAddress -Value $($Adapter.MacAddress)
-        $Interface | Add-Member -MemberType NoteProperty -Name IPAddress -Value $($IPDetails.IPAddress)
-        $InterfaceDetails.Add($Interface) 
-    }
+            $IPDetails = Get-NetIPAddress -InterfaceIndex $($Adapter.InterfaceIndex) | Where-Object { $_.PrefixOrigin -eq "Manual" -or $_.PrefixOrigin -eq "DHCP"}
+            $Interface = New-Object psobject
+            $Interface | Add-Member -MemberType NoteProperty -Name Name -Value $($Adapter.Name)
+            $Interface | Add-Member -MemberType NoteProperty -Name MacAddress -Value $($Adapter.MacAddress)
+            $Interface | Add-Member -MemberType NoteProperty -Name IPAddress -Value $($IPDetails.IPAddress) 
+            [void]$InterfaceDetails.Add($Interface) 
+        }
 
         return $InterfaceDetails
 }
 
-Get-NicIPs | ConvertTo-Json
+Get-NicIPs 
 
