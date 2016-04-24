02541f0bd19f912640c4cd2cd27aef4968649289
Michael Foley <roleyfoley@gmail.com>
2016-04-24 12:47:42 +1000

    Rename Get interface to better suit purpose

--- OS/Get-NetworkDetails.ps1
+++ OS/Get-NetworkDetails.ps1
@@ -1,4 +1,4 @@
-function Get-NicIPs {
+function Get-InterfaceIPs {
         $InterfaceDetails = New-Object System.Collections.ArrayList
         $Adapters = Get-NetAdapter -Physical | Where-Object { $_.Status -eq "Up" } 
                
@@ -6,13 +6,24 @@ function Get-NicIPs {
             $IPDetails = Get-NetIPAddress -InterfaceIndex $($Adapter.InterfaceIndex) | Where-Object { $_.PrefixOrigin -eq "Manual" -or $_.PrefixOrigin -eq "DHCP"}
             $Interface = New-Object psobject
             $Interface | Add-Member -MemberType NoteProperty -Name Name -Value $($Adapter.Name)
+            $Interface | Add-Member -MemberType NoteProperty -Name IFIndex -Value $($Adapter.InterfaceIndex)
             $Interface | Add-Member -MemberType NoteProperty -Name MacAddress -Value $($Adapter.MacAddress)
-            $Interface | Add-Member -MemberType NoteProperty -Name IPAddress -Value $($IPDetails.IPAddress) 
+            $IPArray = New-Object System.Collections.ArrayList
+            foreach ($IP in $IPDetails ) {
+                [void]$IPArray.Add("$($IP.IPAddress)/$($IP.PrefixLength)")
+            }
+            $Interface | Add-Member -MemberType NoteProperty -Name IPAddress -Value $IPArray
+ 
             [void]$InterfaceDetails.Add($Interface) 
         }
 
         return $InterfaceDetails
 }
 
-Get-NicIPs 
+function Get-StaticRouteTable {
+   $StaticRoutes =  get-NetRoute -Protocol NetMgmt -AddressFamily IPv4 
+   
+   return $StaticRoutes
+}
+
 
