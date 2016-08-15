e0f6ef1c7481d485ebde3ead8a5d00286b86ca9f
Michael Foley <roleyfoley@gmail.com>
2016-08-15 11:33:36 +1000

    Update DSC_HostFile.ps1

--- DSC/DSC_HostFile.ps1
+++ DSC/DSC_HostFile.ps1
@@ -1,86 +1,70 @@
-set-location C:\Users\Michael\Source\Repos\PowerShell_NetConfig\DSC
-
 Configuration SpazHome_NetConfig_BasicSetup
 { 
-  import-dscresource -Module xNetworking
+    param(
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$NodeName,
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$Role,
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$IntAlias,
+        [String]$PubIntAlias
+    ) 
+    # Add Extra DSC Resources
+    import-dscresource -Module xNetworking
 
-  node $AllNodes.NodeName 
-  {       
-      xDHCPClient DisableDHCPIpv4
-      {
-          State = 'Enabled'
-          InterfaceAlias = $Node.IntAlias
-          AddressFamily = 'IPv4'
-      }
-      xDHCPClient DisableDHCPIpv6
-      {
-          State = 'Enabled'
-          InterfaceAlias = $Node.IntAlias
-          AddressFamily = 'IPv6'
-      }
-  }
+    Node $NodeName 
 
-  node $AllNodes.Where{$_.Role -eq "Application"}.NodeName
-  { 
-      # Interface Configuration
-      xDefaultGatewayAddress Pub_NoIpv4Gateway
-      {
-          AddressFamily = 'IPv4'
-          InterfaceAlias = $Node.PubIntAlias
-      }
-      xDefaultGatewayAddress Pub_NoIpv6Gateway {
-          AddressFamily =  'IPv6'
-          InterfaceAlias = $Node.PubIntAlias 
-      }
-      xNetConnectionProfile Pub_IntProfile {
-          InterfaceAlias = $Node.PubIntAlias
-          NetworkCategory = 'Public'
-          IPv4Connectivity = 'LocalNetwork'
-      }
-      
-      xFirewall Permit_IIS {
-          Name = 'IIS-WebServerRole-HTTP-In-TCP'
-          Ensure = 'Present'
-          Enabled = 'True'
-          DependsOn = '[WindowsFeature]Add_IIS'
-      }
-      
-      
-
-      # Feature Installaion
-      WindowsFeature Add_IIS {
-          Name = "Web-Server"
-          Ensure = 'Present'
-      }
-      
-  }
-}
-
-# Node Configuration Data Goes here 
-$ConfigData = 
-@{
-    AllNodes = 
-    @(
-
-        @{
-            NodeName = '192.168.2.33'
-            Role = 'Application'
-            IntAlias = 'Ethernet0'
-            PubIntAlias = 'Ethernet1'
+    if ( $Role -eq "Application") { 
+        # Interface Configuration
+        # - Removes Default Gateway from Pub Interface
+        xDefaultGatewayAddress Pub_NoIpv4Gateway
+        {
+            AddressFamily = 'IPv4'
+            InterfaceAlias = $PubIntAlias
         }
-    );
-     NonNodeData = ""  
+        xDefaultGatewayAddress Pub_NoIpv6Gateway {
+            AddressFamily =  'IPv6'
+            InterfaceAlias = $PubIntAlias 
+        }
+
+        xRoute Pub_TMGRoute {
+        AddressFamily = 'IPv4'
+        InterfaceAlias = $PubIntAlias
+        DestinationPrefix = '10.11.249.0/24'
+        NextHop = '10.1.1.1'
+        }
+
+        # - Sets the Network Connection Profile to Public Firewall 
+        xNetConnectionProfile Pub_IntProfile {
+            InterfaceAlias = $Node.PubIntAlias
+            NetworkCategory = 'Public'
+            IPv4Connectivity = 'LocalNetwork'
+        }
+
+        # - Permit IIS Traffic 
+        xFirewall Permit_IIS {
+            Name = 'IIS-WebServerRole-HTTP-In-TCP'
+            Ensure = 'Present'
+            Enabled = 'True'
+            DependsOn = '[WindowsFeature]Add_IIS'
+        }
+
+        # Feature Installaion
+        WindowsFeature Add_IIS {
+            Name = "Web-Server"
+            Ensure = 'Present'
+        }
+
+    }
 }
 
+# This creates the Configuration File 
+Write-Output "Creating Configuration "
+SpazHome_NetConfig_BasicSetup -NodeName 'ZPRITST001' -Role 'Application' -IntAlias 'Ethernet0' -PubIntAlias 'Ethernet1'
 
-$User = 'Administrator'
-$Pass = 'S0mething!'
-
-$secpasswd = ConvertTo-SecureString $Pass -AsPlainText -Force
-$Creds = New-Object System.Management.Automation.PSCredential ($User, $secpasswd)
-
-Write-Output "Creating Configuration"
-SpazHome_NetConfig_BasicSetup -ConfigurationData $ConfigData 
-
+# This pushed the configuration file to the Computer and Applies it
 write-output "ApplyConfiguration "
-Start-DscConfiguration -Path SpazHome_NetConfig_BasicSetup -Wait -Verbose -Force -Credential $Creds -ComputerName 192.168.2.33
+Start-DscConfiguration -Path SpazHome_NetConfig_BasicSetup -Wait -Verbose -Force -Credential $Creds -ComputerName ZPRITST001
